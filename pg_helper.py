0424c7553f311f1e5f2d2f2f1f4e0a3b0c69619b
Khaw SL <khawslee@gmail.com>
2021-01-30 23:01:42 +0800

    Feature: PostgreSQL helper class

---
+++ pg_helper.py
@@ -0,0 +1,68 @@
+import psycopg2
+import sys
+import pandas as pd
+from io import StringIO
+
+# Connection parameters for PostgreSQL DataWarehouse
+main_param_dic = {
+    "host"      : "localhost",
+    "database"  : "datawarehouse",
+    "user"      : "postgres",
+    "password"  : "xxxxxx"
+}
+
+def postgresql_connect():
+    """ Connect to the PostgreSQL database server """
+    conn = None
+    try:
+        # connect to the PostgreSQL server
+        print('Connecting to the PostgreSQL database...')
+        conn = psycopg2.connect(**main_param_dic)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+        sys.exit(1)
+    print("Connection successful")
+    return conn
+
+def postgresql_to_dataframe(conn, select_query, column_names):
+    """
+    Tranform a SELECT query into a pandas dataframe
+    """
+    cursor = conn.cursor()
+    try:
+        cursor.execute(select_query)
+    except (Exception, psycopg2.DatabaseError) as error:
+        print("Error: %s" % error)
+        cursor.close()
+        return 1
+
+    # Naturally we get a list of tupples
+    tupples = cursor.fetchall()
+    cursor.close()
+
+    # We just need to turn it into a pandas dataframe
+    df = pd.DataFrame(tupples, columns=column_names)
+    return df
+
+def copy_from_stringio(conn, df, table):
+    """
+    Save the dataframe in memory
+    and use copy_from() to copy it to the table
+    """
+    # save dataframe to an in memory buffer
+    buffer = StringIO()
+    df.to_csv(buffer, index_label='id', header=False)
+    buffer.seek(0)
+
+    cursor = conn.cursor()
+    try:
+        cursor.copy_from(buffer, table, sep=",")
+        conn.commit()
+    except (Exception, psycopg2.DatabaseError) as error:
+
+        print("Error: %s" % error)
+        conn.rollback()
+        cursor.close()
+        return 1
+    print("copy_from_stringio() done")
+    cursor.close()
